9a3839b2d50b9af90f2a6a1312df25b50ed3186e
Rodrigo Marchesini <rodrigo.marchesini@movile.com>
2016-06-01 10:07:20 -0300

    simple stamp function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var crypto = require('crypto');
 
+function stamp() {
+  var len = 8;
+  return crypto.randomBytes(Math.ceil(len / 2))
+    .toString('hex')
+    .slice(0, len);
+}
+
+module.exports = stamp;
